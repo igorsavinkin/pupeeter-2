def4498c68d40ea36d0737d956764092ef5b0e60
Igor Savinkin <igor.savinkin@gmail.com>
2019-06-07 12:34:04 +0300

    env vars test

---
+++ env_vars_test.js
@@ -0,0 +1,10 @@
+const Apify = require('apify');
+const puppeteer = require('puppeteer'); 
+process.env.APIFY_LOCAL_STORAGE_DIR="./apify_storage";
+process.env.APIFY_MEMORY_MBYTES = 2000;
+
+Apify.main(async () => { 
+	const  enviroment_vars  = await Apify.getEnv();
+	console.log('enviroment vars :', enviroment_vars);
+	//process.exit();
+}
