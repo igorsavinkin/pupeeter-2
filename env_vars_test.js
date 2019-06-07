dac23ff9ffaf5682a9daff4f16eaf123d29a3a1c
Igor Savinkin <igor.savinkin@gmail.com>
2019-06-07 12:57:45 +0300

    commented env. vars in the code

--- env_vars_test.js
+++ env_vars_test.js
@@ -1,7 +1,7 @@
 const Apify = require('apify');
 const puppeteer = require('puppeteer'); 
-process.env.APIFY_LOCAL_STORAGE_DIR="./apify_storage";
-process.env.APIFY_MEMORY_MBYTES = 2000;
+//process.env.APIFY_LOCAL_STORAGE_DIR="./apify_storage";
+//process.env.APIFY_MEMORY_MBYTES = 2000;
 
 Apify.main(async () => { 
 	const  enviroment_vars  = await Apify.getEnv();
